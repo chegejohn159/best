c38fdecc366f5144263b91b72c74b3af3ec75d11
John Kamau <chegejohn159@gmail.com>
2020-08-16 14:09:00 +0300

    Add files via upload

---
+++ private-key/priv/ceaser.py
@@ -0,0 +1,20 @@
+message = 'Hello everyone Corona is real lets keep safe.'
+key = 4
+mode = 'encrypt'
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
+translated = ''
+for symbol in message:
+	if symbol in SYMBOLS:
+		symbolIndex = SYMBOLS.find(symbol)
+		if mode == 'encrypt':
+			translatedIndex = symbolIndex + key
+		elif mode == 'decrypt':
+			translatedIndex = symbolIndex - key
+		if translatedIndex >= len(SYMBOLS):
+			translatedIndex = translatedIndex - len(SYMBOLS)
+		elif translatedIndex < 0:
+			translatedIndex = translatedIndex + len(SYMBOLS)
+		translated = translated + SYMBOLS[translatedIndex]
+	else:
+		translated = translated + symbol
+print(translated)
